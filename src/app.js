2e8a54cfe639463e34251d57784bc85638aa7c8c
kevinco26 <kevin.cohen26@gmail.com>
2015-04-04 02:00:29 -0400

    Working with pebble SDK. One reminder only

--- src/app.js
+++ src/app.js
@@ -1,91 +1,92 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
 
 var UI = require('ui');
-var Wakeup = require('wakeup');
-var Clock = require('clock');
-var ajax = require('ajax');
-var nextTime;
 
-var i = 0;
+var Wakeup = require('wakeup');
+var ajax = require('ajax');
+var Clock = require('clock');
+
+var medicine;
+var nextTime;
+var hours;
+var minutes;
 
 var main = new UI.Card({
-  title: '',
+  title: 'Welcome to your medicine tracker',
   subtitle: '',
-  body: '',
-  
+  body: 'Press the down button to schedule the next reminder'
 });
 
-ajax(
+
+ ajax(
   {
-    url: 'http://client-75-102-97-252.mobility-up.psu.edu:3000/Kevin/medicine',
+    url: 'http://Kevin-Cs-MacBook-Pro-4.local:3000/Kevin/medicine',
     type: 'json'
   },
   function(data, status, request) {
-    nextTime = Clock.weekday(data.contents[3].weekday, data.contents[1].hour,data.contents[2].minutes);
-    
-    Wakeup.schedule(
-    {
-        time: nextTime,
-    });
-    main.title('Medicine: ' + data.contents[0].medicine);
-    main.subtitle('At: '+ data.contents[1].hour + ':' + data.contents[2].minutes + (data.contents[1].hour<=12?'AM':'PM'));
-    main.body('On:' +  data.contents[3].weekday);
-     
+    medicine = data.contents.medicine;
+    nextTime = Clock.weekday(data.contents.weekday,data.contents.hour,data.contents.minutes);
+    hours = data.contents.hour;
+    minutes = data.contents.minutes;
   },
   function(error, status, request) {
     console.log('The ajax request failed: ' + error);
   }
 );
 
+// wakeup get launch event
 Wakeup.launch(function(e) {
-    
-      ajax(
+  if (e.wakeup) {
+   // Wakeup.cancel('all');
+    var Vibe = require('ui/vibe');
+    var Light = require('ui/light');
+  
+    Vibe.vibrate('double'); 
+    Light.on('double');
+    console.log('woke up!' + JSON.stringify(e));
+    main.title('Medicine: ' + medicine);
+    main.body('At: '+ hours + ':' + (minutes<10?'0'+minutes:minutes) + (hours<=11?'AM':'PM'));
+
+  } else {
+    ajax(
   {
-    url: 'http://client-75-102-97-252.mobility-up.psu.edu:3000/Kevin/medicine',
+    url: 'http://Kevin-Cs-MacBook-Pro-4.local:3000/Kevin/medicine',
     type: 'json'
   },
   function(data, status, request) {
-    if (e.wakeup) 
-    {
-      i = i+4;
-      console.log('Woke up to ' + e.id + '! data: ' + JSON.stringify(e.data));
-      var Vibe = require('ui/vibe');
-      var Light = require('ui/light');
-  
-      Vibe.vibrate('double'); 
-      Light.on('double');
-      
-      main.title('Medicine: ' + data.contents[i].medicine);
-      main.subtitle('At: '+ data.contents[1+i].hour + ':' + data.contents[2+i].minutes + (data.contents[1+i].hour<=12?'AM':'PM'));
-      main.body('On:' +  data.contents[3+i].weekday);
-      
-      nextTime = Clock.weekday(data.contents[3+i].weekday, data.contents[1+i].hour,data.contents[2+i].minutes);
-      
-      Wakeup.schedule(
-      {
-          time: nextTime,
-      });
-      
-    }
-    else
-    {
-      main.title('');
-      main.subtitle('DO NOT TAKE PILL YET');
-      main.body('');
-    } 
-},
+    medicine = data.contents.medicine;
+    nextTime = Clock.weekday(data.contents.weekday,data.contents.hour,data.contents.minutes);
+    hours = data.contents.hour;
+    minutes = data.contents.minutes;
+  },
   function(error, status, request) {
     console.log('The ajax request failed: ' + error);
   }
-);   
+);
+     main.body('Next medicine: ' + medicine + 'At: '+ hours + ':' + (minutes<10?'0'+minutes:minutes) + (hours<=11?'AM':'PM'));
+  }
 });
+ 
+ 
 
+main.on('click', 'down', function(e) {
+  // schedule a wakeup, takes Date or seconds (not ms) and persists data
+  Wakeup.cancel('all');
+
+  Wakeup.schedule(
+    {
+      time: nextTime,
+      data: { hello: 'world' },
+    },
+    function(e) {
+      console.log('wakeup set! ' + JSON.stringify(e));
+ 
+      if (e.failed) {
+        main.body('Wakeup failed: ' + e.error + '!');
+      } else {
+        main.body('Wakeup set!\nExit or cancel.');
+      }
+    }
+  );
+});
+ 
 main.show();
-
-// if clicks select, starts schedule for next available pill for that same day. If none, display No more pills for today.
-
-
